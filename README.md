abb38a9aaff850a605b79f70bb78868babe06ac3
hvanveen <hvveen@gmail.com>
2018-11-01 19:26:30 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AzureRMDeployment
-An Azure DevOps extension for deploying ARM templates
+
+This extension uses the Azure RM PowerShell New-AzureRmDeployment cmdlet for installing any ARM template in Azure DevOps.
+
+If the specified json parameter file contains the parameter 'ProductName', it will use it as deploymentname (after converting spaces to hyphens).
+If the parameter is not found, the resourcegroup name will be used as deploymentname (again after converting spaces to hyphens).
+
+## Notes
+
+This extension requires the Agent to have "Azure RM PowerShell 6.10.0" or higher installed
+The New-AzureRmDeployment cmdlet does not exist in earlier versions!
+
+## Help & Contact
+
+The source for this extension can be found at https://github.com/hansvanveen/AzureRMDeployment
+Support requests can be send to hvveen@gmail.com
---
+++ azurermdeploymentReleaseTask/azurermdeployment.ps1
@@ -0,0 +1,37 @@
+Write-Output "Starting AzureRmDeployment task (v1.2.0)"
+
+Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_ -Force
+Initialize-Azure
+
+#  Get primary input from extension
+$resourceGroupName = Get-VstsInput -Name resourceGroupName -Require
+$resourceLocation  = Get-VstsInput -Name resourceLocation  -Require
+$templateLocation  = Get-VstsInput -Name templateLocation  -Require
+$extendedLogging   = Get-VstsInput -Name extendedLogging   -Require
+
+if ($extendedLogging) {
+  Write-Output ">> ResourceGroup name: $resourceGroupName"
+  Write-Output ">> Resource location : $resourceLocation"
+  Write-Output ">> Template location : $templateLocation"
+}
+
+#  Get secondary input from extension (depends on the primary)
+#  Let the cmdlet figure out whether the Files/Uri's exist
+  if ($templateLocation -eq 'Linked artifact') {
+  $templateFile  = Get-VstsInput -Name csmFile -Require
+  $parameterFile = Get-VstsInput -Name csmParametersFile -Require
+  $cfg = Get-Content "$parameterFile" | ConvertFrom-Json
+  $deploymentName = ($cfg.parameters.ProductName.value).Replace(' ', '-')
+  if ($deploymentName.length -eq 0) { $deploymentName = $resourceGroupName.Replace(' ', '-') }
+  $result = New-AzureRmDeployment -Name $deploymentName -Location "$resourceLocation" -TemplateFile "$templateFile" -TemplateParameterFile "$parameterFile" -Verbose
+}
+else {
+  $templateUri  = Get-VstsInput -Name csmFileLink -Require
+  $parameterUri = Get-VstsInput -Name csmParametersFileLink -Require
+  $cfg = (Invoke-WebRequest -Uri "$parameteruri" -UseBasicParsing).Content  | ConvertFrom-Json
+  $deploymentName = ($cfg.parameters.ProductName.value).Replace(' ', '-')
+  if ($deploymentName.length -eq 0) { $deploymentName = $resourceGroupName.Replace(' ', '-') }
+  $result = New-AzureRmDeployment -Name $deploymentName -Location "$resourceLocation" -TemplateUri "$templateUri" -TemplateParameterUri "$parameterUri" -Verbose
+}
+
+if ($extendedLogging) { Write-Output $result }
